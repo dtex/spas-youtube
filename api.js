b2fc51564ac26346088512b64bad8a0aa5af78c9
Donovan Buck <donovan@donovan.bz>
2013-04-28 21:14:44 -0500

    Update to use spas-http

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-var spasRequest = require("../spas-request"),
+var spashttp = require("spas-http"),
 	_ = require("underscore")._;
 	
 exports["custom"] = {	
@@ -6,14 +6,14 @@ exports["custom"] = {
 		
 		params.url = "http://gdata.youtube.com/feeds/api/videos?v=2";
 		
-		spasRequest.request(params, credentials, function( err, videos ) {
+		spashttp.request(params, credentials, function( err, videos ) {
 			
 			if (_.has(videos, 'feed')) {
 				
 				var n = videos.feed.entry.length;
 				
 				_.each(videos.feed.entry, function( obj, key) {
-					spasRequest.request({url: "http://gdata.youtube.com/feeds/api/videos/" + obj.media$group.yt$videoid.$t + '?v=2&alt=json'}, credentials, function( err, video ) {
+					spashttp.request({url: "http://gdata.youtube.com/feeds/api/videos/" + obj.media$group.yt$videoid.$t + '?v=2&alt=json'}, credentials, function( err, video ) {
 						n=n-1;
 						if(_.has(video, 'entry')) {
 							videos.feed.entry[key].media$group.media$keywords.$t = video.entry.media$group.media$keywords.$t;
